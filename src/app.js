9b0d277d2027c831977abc0ba332dcdcdfd2af5f
todendvv91 <todendvv91@gmail.com>
2023-09-26 14:32:22 +0900

    test

--- src/app.js
+++ src/app.js
@@ -1,4 +1,6 @@
 import express from "express";
+import viewRouter from "./router/viewRouter";
+import apiRouter from "./router/apiRouter";
 
 const app = express();
 
@@ -21,17 +23,13 @@ app.set("view engine", "ejs");
  * ejs의 파일의 위치는 이프로젝트의 src 폴더에 client 폴더에 html 폴더 안에 있어
  */
 app.set("views", process.cwd() + "/src/client/html");
+/**
+ * 주소 : /**,   view 만 전달해주는 router viewRouter -> ejs 파일만 전달해주는 router
+ * 주소 : /api/**  api 만 전달해주는 router apiRouter -> 데이터만 전달해주는 router
+ */
 
-app.get("/", (request, response) => {
-  const homeData = {
-    data: [{ name: "철수" }, { name: "영희" }, { name: "민수" }],
-  };
-  response.render("home", homeData);
-});
-
-app.get("/introduce", (request, response) => {
-  response.render("introduce");
-});
+app.use("/api", apiRouter);
+app.use("/", viewRouter);
 
 app.listen(8080, () => {
   console.info("8080 포트 서버 열림 http://localhost:8080");
