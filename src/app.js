1eca95e52d42f479893e4277bb7ff08e620cb8ee
todendvv91 <todendvv91@gmail.com>
2023-09-26 13:52:01 +0900

    test

--- src/app.js
+++ src/app.js
@@ -2,24 +2,37 @@ import express from "express";
 
 const app = express();
 
-// 노드는 middleware 시스템으로 이루어져있음
+/**
+ * node는 middleware 시스템으로 이루어져있음
+ */
 
-// express에게 ejs를 사용할 것이라고 선언
+/**
+ * ejs 템플릿 엔진
+ * 구멍이 있는 페이지 -> 구멍에 데이터를 넣을 수 있는 것
+ * node ejs
+ * npm install ejs
+ */
+
+/**
+ * express야 나 ejs 쓸거야
+ */
 app.set("view engine", "ejs");
-// 위치는 절대 주소를 지정하고 파일 위치를 잡는다.
+/**
+ * ejs의 파일의 위치는 이프로젝트의 src 폴더에 client 폴더에 html 폴더 안에 있어
+ */
 app.set("views", process.cwd() + "/src/client/html");
 
-app.get("/", (req, res) => {
+app.get("/", (request, response) => {
   const homeData = {
     data: [{ name: "철수" }, { name: "영희" }, { name: "민수" }],
   };
-  res.render("home", homeData);
+  response.render("home", homeData);
 });
 
-app.get("/abc", (req, res) => {
-  res.send("abc로 들어왔습니다!!!");
+app.get("/introduce", (request, response) => {
+  response.render("introduce");
 });
 
 app.listen(8080, () => {
-  console.info("8080 포트 서버 열림 http://localhost:8080 👍");
+  console.info("8080 포트 서버 열림 http://localhost:8080");
 });
